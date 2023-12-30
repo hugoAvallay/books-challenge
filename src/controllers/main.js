20425109e9a5f30a3a2197aea2d7f2ced9f714c4
hugoAvallay <h40hugo@gmail.com>
2023-12-30 17:47:47 -0300

    Refactor authorBooks view & controller, and update bookDetail view & controller for displaying details of each book.

--- src/controllers/main.js
+++ src/controllers/main.js
@@ -11,10 +11,30 @@ const mainController = {
       })
       .catch((error) => console.log(error));
   },
-  bookDetail: (req, res) => {
+  /* ------------------------------------------------------------------------------- */
+  bookDetail: async (req, res) => {
     // Implement look for details in the database
-    res.render('bookDetail');
+
+    try {
+      const book = await db.Book.findByPk(req.params.id, {
+        include: [{ association: 'authors' }]
+      })
+
+      if (!book) {
+        return res.status(404).send('Libro no encontrado');
+      }
+
+      return res.render('bookDetail', {
+        book
+      })
+
+    } catch (error) {
+      console.error(error);
+      res.status(500).send('Error interno del servidor');
+    }
   },
+
+  /* --------------------------------------------------------------------- */
   bookSearch: (req, res) => {
     res.render('search', { books: [] });
   },
@@ -33,42 +53,60 @@ const mainController = {
       })
       .catch((error) => console.log(error));
   },
-  authorBooks: (req, res) => {
-    // Implement books by author
-    res.render('authorBooks');
-  },
-  register: (req, res) => {
-    res.render('register');
-  },
-  processRegister: (req, res) => {
-    db.User.create({
-      Name: req.body.name,
-      Email: req.body.email,
-      Country: req.body.country,
-      Pass: bcryptjs.hashSync(req.body.password, 10),
-      CategoryId: req.body.category
-    })
-      .then(() => {
-        res.redirect('/');
-      })
-      .catch((error) => console.log(error));
-  },
-  login: (req, res) => {
-    // Implement login process
-    res.render('login');
-  },
-  processLogin: (req, res) => {
-    // Implement login process
-    res.render('home');
-  },
-  edit: (req, res) => {
-    // Implement edit book
-    res.render('editBook', {id: req.params.id})
-  },
-  processEdit: (req, res) => {
-    // Implement edit book
-    res.render('home');
-  }
-};
 
-module.exports = mainController;
+  /* ------------------------------------------------------------------------ */
+
+  authorBooks: async (req, res) => {
+    // Implement books by author
+    try {
+      const author = await db.Author.findByPk(req.params.id, {
+        include: [{ model: db.Book, as: 'books' }],
+      });
+    
+      if (!author) {
+        return res.status(404).send('Autor no encontrado');
+      }
+    
+      res.render('authorBooks', { author });
+    } catch (error) {
+      console.error(error);
+      res.status(500).send('Error interno del servidor');
+    }
+        
+      }, 
+    /* ------------------------------------------------------------------------------- */
+    register: (req, res) => {
+      res.render('register');
+    },
+      processRegister: (req, res) => {
+        db.User.create({
+          Name: req.body.name,
+          Email: req.body.email,
+          Country: req.body.country,
+          Pass: bcryptjs.hashSync(req.body.password, 10),
+          CategoryId: req.body.category
+        })
+          .then(() => {
+            res.redirect('/');
+          })
+          .catch((error) => console.log(error));
+      },
+        login: (req, res) => {
+          // Implement login process
+          res.render('login');
+        },
+          processLogin: (req, res) => {
+            // Implement login process
+            res.render('home');
+          },
+            edit: (req, res) => {
+              // Implement edit book
+              res.render('editBook', { id: req.params.id })
+            },
+              processEdit: (req, res) => {
+                // Implement edit book
+                res.render('home');
+              }
+  };
+
+  module.exports = mainController;
